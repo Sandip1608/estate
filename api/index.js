077466011c44316c9632cdf7569d9e701612924a
Sandip1608 <sndpbhujl168@gmail.com>
2023-10-26 18:23:07 +0545

    signup page completed

--- api/index.js
+++ api/index.js
@@ -26,12 +26,13 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 
 
+
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || 'Internal Server Error .....'
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    sucess: false,
+    success: false,
     statusCode,
-    message
+    message,
   });
-})
+});
