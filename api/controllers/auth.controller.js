4ae782d2f76c798abf9ee470cb14752ab49a211c
Sandip1608 <sndpbhujl168@gmail.com>
2023-10-26 19:17:29 +0545

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async(req, res, next) => {
     const {username, email, password} = req.body
@@ -12,4 +15,19 @@ export const signup = async(req, res, next) => {
         next(err)
     }
 
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} =req.body
+    try{
+        const validEmail = await User.findOne({email})
+        if (!validEmail) return next(errorHandler(404, 'email not found !!!'))
+        const validPassword = bcryptjs.compareSync(password, validEmail.password)
+        if (!validPassword) return next(errorHandler(404, 'password didnt match !!!'))
+        const token = jwt.sign({id: validEmail._id}, process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validEmail._doc
+        res.cookie('access_token', token, {httpOnly:true}).status(200).json(rest)
+    }catch(err){
+        next(err)
+    }
 }
